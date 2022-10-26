5733174e1b1ec61a5544d010313fb5cce9bd7be6
Ben Strathdee <ben.strathdee@live.com.au>
2022-10-26 16:59:16 +1100

    updated sample data

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,6 @@
 INSERT INTO temps (first_name, last_name, left_val, right_val) VALUES ("Claire", "test", 1, 28), ("Steve", "test", 2, 7), ("Sarah", "test", 8, 17), ("John", "test", 18, 27), ("Nathan", "test", 3, 4), ("Paula", "test", 5, 6), ("Anne", "test", 9, 10), ("Roger", "test", 11, 12), ("Tim", "test", 13, 14), ("Peter", "test", 15, 16), ("Hugo", "test", 19, 26), ("Matthew", "test", 20, 21), ("Ruth", "test", 22, 23), ("Simon", "test", 24, 25);
 
-INSERT INTO jobs (name, start_date, end_date, temp_id) VALUES ("job1", 0, 2, null), ("job2", 1, 4, null), ("job3", 2, 7, null), ("job4", 4, 5, null), ("job5", 5, 8, null), ("job6", 7, 9, null), ("job7", 3, 6, null);
+INSERT INTO jobs (name, start_date, end_date, temp_id) VALUES ("job1", 0, 2, 1), ("job2", 1, 4, 4), ("job3", 2, 7, 3), ("job4", 4, 5, 5), ("job5", 5, 8, 2), ("job6", 7, 9, null), ("job7", 3, 6, null);
 
 INSERT INTO users (username, email, password, role, fingerprint) VALUE ("admin", "admin@admin.com", "$2a$10$2v.1GZN5ufgD00DzEpcAdetl9G3bnTSNeb6uq1FLOCsFNZJSZd4Ni", "admin", "3e18b4fe-f561-4242-b41e-6074320b294c");
 
--- src/test/java/me/strathdee/assignmentapi/AssignmentApiApplicationTests.java
+++ src/test/java/me/strathdee/assignmentapi/AssignmentApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.assignmentapi;
+package me.strathdee.assignmentapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
